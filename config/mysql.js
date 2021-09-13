908887b18f143e8e80336bed52ac4407be2d2199
lxp <liaoxinpeng@chainsuns.com>
2021-09-13 16:56:31 +0800

    2021/09/13 vercel-test

--- config/mysql.js
+++ config/mysql.js
@@ -1,10 +1,10 @@
 // Require and initialize outside of your main handler
 const mysql = require('serverless-mysql')({
     config: {
-        host: '172.16.1.230',
+        host: 'rm-3nsbo8xy0h258g04yjo.mysql.rds.aliyuncs.com',
         database: 'router_events',
-        user: 'game',
-        password: 'winter123'
+        user: 'nullsw',
+        password: 'ChXWZmmccsdhiStSI4'
     }
 })
 
