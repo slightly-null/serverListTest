0b83533c8482065aa956fd627c464f91b9fd8324
lxp <liaoxinpeng@chainsuns.com>
2021-09-13 17:30:53 +0800

    2021/09/13 vercel-test

--- api/test111.js
+++ api/test111.js
@@ -1,4 +1,13 @@
-const mysql = require('../config/mysql')
+// const mysql = require('../config/mysql')
+
+const mysql = require('serverless-mysql')({
+    config: {
+        host     : 'rm-3nsbo8xy0h258g04yjo.mysql.rds.aliyuncs.com',
+        database : 'test1_nulls_world',
+        user     : 'nullsw',
+        password : 'ChXWZmmccsdhiStSI4'
+    }
+})
 
 module.exports = async (req, res) => {
     // Run your query
