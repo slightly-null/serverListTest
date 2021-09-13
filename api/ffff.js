845ec0bf11f3c25cd15524464ac9478edf152256
lxp <liaoxinpeng@chainsuns.com>
2021-09-13 17:24:53 +0800

    2021/09/13 vercel-test

--- api/ffff.js
+++ api/ffff.js
@@ -1,13 +1,7 @@
 const mysql = require('../config/mysql')
 
-// Main handler function
-exports.handler = async (req, res) => {
+module.exports = async (req, res) => {
     // Run your query
     let results = await mysql.query('select * from config')
-
-    // Run clean up function
-    await mysql.end()
-
-    // Return the results
-    return results
+    res.status(200).json(results);
 }
