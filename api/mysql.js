88cb3d35cc8b42d8fd9e692ab883291af39d5553
lxp <liaoxinpeng@chainsuns.com>
2021-09-13 18:05:41 +0800

    2021/09/13 vercel-test

--- api/mysql.js
+++ api/mysql.js
@@ -1,6 +1,6 @@
 const mysql = require('../config/mysql')
 
 module.exports = async (req, res) => {
-    const data = await mysql.query('select * from config')
+    const data = await mysql.query('select * from NewGame')
     res.status(200).json(data);
 }
--- config/mysql.js
+++ config/mysql.js
@@ -1,10 +1,10 @@
 // Require and initialize outside of your main handler
 const mysql = require('serverless-mysql')({
     config: {
-        host: 'rm-3nsbo8xy0h258g04yjo.mysql.rds.aliyuncs.com',
-        database: 'test1_nulls_world',
-        user: 'nullsw',
-        password: 'ChXWZmmccsdhiStSI4'
+        host: 'db4free.net',
+        database: 'router_events',
+        user: 'vercel',
+        password: 'vercel123'
     }
 })
 
