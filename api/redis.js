a0de85c8df3b3dbb863c330b88c4655271de0c6b
lxp <liaoxinpeng@chainsuns.com>
2021-09-14 11:23:26 +0800

    2021/09/13 vercel-test

--- api/redis.js
+++ api/redis.js
@@ -1,7 +1,15 @@
-const redis = require('../config/redis')
 
-module.exports = async (req, res) => {
-    await redis.set('foo', 'bar');
-    const data = await redis.get("foo");
-    return {data: data}
+const redis = require("redis");
+
+module.exports = async(req, res) => {
+    const client = redis.createClient ({
+        url : process.env.REDIS_URL
+    });
+
+    await client.set('foo', 'bar');
+    const data = await client.get("foo");
+
+    res.json({
+        body: data
+    })
 }
