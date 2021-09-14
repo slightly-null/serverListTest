77053462e1997dc45a88fb490ed923856164d2ec
lxp <liaoxinpeng@chainsuns.com>
2021-09-14 15:56:40 +0800

    2021/09/14 vercel-test

--- api/statisticsAll.js
+++ api/statisticsAll.js
@@ -5,9 +5,9 @@ module.exports = async (req, res) => {
     const data = await mysql.transaction()
         .query('SELECT count(DISTINCT NewNonce.player) AS PlayAddresses FROM NewNonce')
         .query((T1) => {
-            const result = ['SELECT count(StatisticsGameId.gameId) AS Games,sum(StatisticsGameId.scenes) AS Scenes,sum(StatisticsGameId.items) AS Items,sum(StatisticsGameId.randoms) AS Randoms,sum(StatisticsGameId.nonces) AS Nonces,T1.Playaddresses FROM StatisticsGameId'];
+            const result = ['SELECT count(StatisticsGameId.gameId) AS Games,sum(StatisticsGameId.scenes) AS Scenes,sum(StatisticsGameId.items) AS Items,sum(StatisticsGameId.randoms) AS Randoms,sum(StatisticsGameId.nonces) AS Nonces FROM StatisticsGameId'];
             return {
-                T1,
+                PlayAddresses: T1.PlayAddresses,
                 result
             }
         })
