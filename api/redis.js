60dbb3769e7c821a8f15d4817182269351db3d05
lxp <liaoxinpeng@chainsuns.com>
2021-09-14 11:11:54 +0800

    2021/09/13 vercel-test

--- api/redis.js
+++ api/redis.js
@@ -3,5 +3,5 @@ const redis = require('../config/redis')
 module.exports = async (req, res) => {
     await redis.set('foo', 'bar');
     const data = await redis.get("foo");
-    res.status(200).json(data);
+    return {data: data}
 }
