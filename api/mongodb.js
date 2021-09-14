3b13f0cf23dad4dc983f4ba57dbd94290629082c
lxp <liaoxinpeng@chainsuns.com>
2021-09-14 14:26:23 +0800

    2021/09/14 vercel-test

--- api/mongodb.js
+++ api/mongodb.js
@@ -3,9 +3,6 @@
 const clientPromise = require('./mongodb-client');
 // Handler
 module.exports = async (req, res) => {
-    // Get the MongoClient by calling await on the promise.
-    // Because it is a promise, it will only resolve once.
     const client = await clientPromise;
-    // Use the client to return the name of the connected database.
     res.status(200).json({dbName: client.db().databaseName});
 }
