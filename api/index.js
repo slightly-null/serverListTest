9079fb65ae37fb7d28a9dfc14d78cea09e44dbe9
lxp <liaoxinpeng@chainsuns.com>
2021-09-14 14:42:45 +0800

    2021/09/14 vercel-test

--- api/index.js
+++ api/index.js
@@ -4,3 +4,18 @@ module.exports = async (req, res) => {
     };
     res.status(200).json(data);
 }
+
+module.exports.hello = async (req, res) => {
+    const data = {
+        msg: "hello world!"
+    };
+    res.status(200).json(data);
+}
+
+
+module.exports.test = async (req, res) => {
+    const data = {
+        msg: "hello world!"
+    };
+    res.status(200).json(data);
+}
