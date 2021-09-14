b21d64273bc872b242b44adf0c3f7f3a04e400cf
lxp <liaoxinpeng@chainsuns.com>
2021-09-14 15:52:14 +0800

    2021/09/14 vercel-test

--- api/statisticsAll.js
+++ api/statisticsAll.js
@@ -2,8 +2,17 @@ const mysql = require('../config/mysql')
 const Result = require('../constants/result')
 
 module.exports = async (req, res) => {
-    const sql = 'SELECT count(StatisticsGameId.gameId) AS Games,sum(StatisticsGameId.scenes) AS Scenes,sum(StatisticsGameId.items) AS Items,sum(StatisticsGameId.randoms) AS Randoms,sum(StatisticsGameId.nonces) AS Nonces,T1.Playaddresses FROM StatisticsGameId,(SELECT count(DISTINCT NewNonce.player) AS PlayAddresses FROM NewNonce) AS T1';
-    const result = await mysql.query(sql);
-    const data = Result.commonResult(0, result);
-    res.status(200).json(data);
+    const data = await mysql.transaction()
+        .query('SELECT count(DISTINCT NewNonce.player) AS PlayAddresses FROM NewNonce')
+        .query((T1) => {
+            const result = ['SELECT count(StatisticsGameId.gameId) AS Games,sum(StatisticsGameId.scenes) AS Scenes,sum(StatisticsGameId.items) AS Items,sum(StatisticsGameId.randoms) AS Randoms,sum(StatisticsGameId.nonces) AS Nonces,T1.Playaddresses FROM StatisticsGameId'];
+            return {
+                T1,
+                result
+            }
+        })
+        .rollback(e => console.log(e))
+        .commit();
+    const result = Result.commonResult(0, data);
+    res.status(200).json(result);
 }
