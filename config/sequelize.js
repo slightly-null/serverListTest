c7d45f0f629df974e8f051862dffa148e8d3f467
罗鸿清 <15279163025@163.com>
2021-09-22 16:16:22 +0800

    fix

--- config/sequelize.js
+++ config/sequelize.js
@@ -4,7 +4,8 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('test1_nulls_world', 'nullsw', 'ChXWZmmccsdhiStSI4', {
     host: 'rm-3nsbo8xy0h258g04yjo.mysql.rds.aliyuncs.com',
     port:3306,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectModule: require('mysql2'),
   });
 
   async function test(){
